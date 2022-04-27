d74eb58de3d6c5cd5ef6b8b7fa41a936fcb54004
Nedas.Kisi <nedas.kisi@gmail.com>
2022-04-28 00:58:55 +0300

    added read function

--- script.js
+++ script.js
@@ -17,4 +17,26 @@ addItemButton.addEventListener("click", (e) => {
    itemsArray.push({value: text.value, done: false});
    text.value = "";
    localStorage.setItem("shopItem", JSON.stringify(itemsArray));
+   readItems();
 });
+
+function readItems() {
+   itemsArray = getItems() ? JSON.parse(getItems()) : [];
+   currentIndex = itemsArray.length;
+
+   let htmlCode = "";
+   itemsArray.forEach((item, ind) => {
+     htmlCode += 
+     `
+     <div class='itemsLi'>
+     <input onchange='toggle(${ind})' type='checkbox' id=check-${ind} ${item.done ? 'checked' : ''}>
+     <p class='itemText'>${item.value}</p>
+     <button onclick='edit(${ind})' class='edit__button'><i class="fa fa-pencil-square-o fa-2x" aria-hidden="true" id="edit__icon"></i></button>
+     <button onclick='deleteshopItem(${ind})' class='deleteButton'><i class="fa fa-trash fa-2x" aria-hidden="true" id="bin__icon"></i></button>
+     </div>
+     `;
+   });
+   listBox.innerHTML = htmlCode;
+}
+
+readItems()
